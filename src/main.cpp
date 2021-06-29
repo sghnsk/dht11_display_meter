5f82284d065981d677dfa87145777313dd81825b
sghnsk <sgh.nsk@gmail.com>
2021-06-29 14:36:26 +0700

    Добавил ds18b20
    - dht11 работает нормльно только до 50 градусов

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 #include <SevSeg.h>
 #include <DHT.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#define DHTPIN A0
-DHT dht(DHTPIN, DHT11);
+DHT dht(A0, DHT11);
+
+OneWire oneWire(A1);
+DallasTemperature ds(&oneWire);
+unsigned long delayInMillis = 750;
+unsigned long lastTempRequest = 0;
 
 SevSeg sevseg;
 
@@ -13,11 +19,21 @@ void setup() {
     byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
     sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins, true);
     sevseg.setBrightness(100);
+
     dht.begin();
+
+    ds.begin();
+    ds.setWaitForConversion(false);
 }
 
 void loop() {
-    int temp = dht.readTemperature();
+    int temp;
+    if (millis() - lastTempRequest >= delayInMillis) {
+        ds.requestTemperatures();
+        temp = ds.getTempCByIndex(0);
+        lastTempRequest = millis(); 
+    }
+    // int temp = dht.readTemperature();
     int hum = dht.readHumidity();
     char outBuff[8];
     sprintf(outBuff, "%d.%d", temp, hum);
